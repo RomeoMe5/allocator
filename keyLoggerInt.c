617f32ffbe1ae7e08f9f8b433c38f85417d97f81
RomeoMe5 <romeomea@gmail.com>
2016-12-06 00:08:50 +0300

    Update keyLoggerInt.c

--- keyLoggerInt.c
+++ keyLoggerInt.c
@@ -1,4 +1,11 @@
-﻿#include <sys/types.h>
+/*Задача: реализовать демон, снимающий статистику с клавиатуры и записывающий ее в файл. Например, частоту нажатий клавиш за период в 5 минут.
+события с клавиатуры можно брать тут: /dev/input/eventX (или удобнее: /dev/input/by-path/… - там будет понятно, что из этого клавиатура)
+Описание событий: https://www.kernel.org/doc/Documentation/input/input.txt
+демон реализовать двумя способами: самостоятельно через fork() и через systemd
+про первый способ посмотреть можно тут:http://www.netzmafia.de/skripten/unix/linux-daemon-howto.html
+про второй: https://www.freedesktop.org/software/systemd/man/systemd.service.html
+*/
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
